f066ff11e55ab6259b0c24679f8927efec8c922a
Max Vicinelli <maxvicinelli11@gmail.com>
2022-01-28 15:26:56 -0500

    first commit for DayKeeper. Setting up xCode project

---
+++ DayKeeperApp/DayKeeperApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DayKeeperApp
+//
+//  Created by Max Vicinelli on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to DayKeeper")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DayKeeperApp/DayKeeperApp/DayKeeperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayKeeperAppApp.swift
+//  DayKeeperApp
+//
+//  Created by Max Vicinelli on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DayKeeperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
