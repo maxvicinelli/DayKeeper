5ace6d5382dcde451ef7453c7d08d7eee29f9e42
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-02-02 14:16:10 -0500

    setting up AI testing and development

---
+++ AI development/AI_calendar.py
@@ -0,0 +1,14 @@
+# Ariel Attias
+# Feb 2, 2022
+
+
+# testing AI
+
+# input:
+# calendar and outcome (late/early)
+
+# output:
+# which notification scheme was utilized
+
+# tests are intended to flesh out which method of increasing or decreasing notification count should be used
+# for now notification scheme will just be numbered by severity (1 through 4).
