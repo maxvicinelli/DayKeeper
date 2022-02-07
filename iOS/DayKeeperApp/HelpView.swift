807fdcb34a85b18ec587a9a01bac2faae3197619
Jonah Kalsner Kershen <jonah.w.kalsner.kershen.22@dartmouth.edu>
2022-02-07 11:43:22 -0500

    created helpview file

---
+++ iOS/DayKeeperApp/HelpView.swift
@@ -0,0 +1,20 @@
+//
+//  HelpView.swift
+//  DayKeeperApp
+//
+//  Created by Jonah Kalsner Kershen on 2/6/22.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text("do you need help?")
+    }
+}
+
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpView()
+    }
+}
