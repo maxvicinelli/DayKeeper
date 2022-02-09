efbf089ca53565f92f0415d3fe10cce62a1025cc
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-02-08 20:02:17 -0500

    starting pre event tasks logic

---
+++ AI development/pre_event_tracking.py
@@ -0,0 +1,8 @@
+# Logic tying tasks to events
+
+# List of tasks:
+# Showering, Brushing teeth
+# putting clothes out
+# preparing things/bag
+# eating breakfast
+# leaving early (does maps do this for us), will be variable based on how late you are
