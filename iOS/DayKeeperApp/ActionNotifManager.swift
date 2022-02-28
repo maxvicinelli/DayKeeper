638ef4fd4d3a3fcbe23ad305546161f2399c340b
Ariel Attias <Ariel.E.Attias.22@dartmouth.edu>
2022-02-28 13:01:48 -0500

    fixed     func userNotificationCenter

--- iOS/DayKeeperApp/ActionNotifManager.swift
+++ iOS/DayKeeperApp/ActionNotifManager.swift
@@ -94,40 +94,40 @@ final class ActionNotifManager: NSObject, UNUserNotificationCenterDelegate {
    
    }
    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {    
+       func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         let event = response.notification.request.content.title
         if let event = getEventsFromDb().first(where: {$0.Title == event}) {
             
             if response.actionIdentifier == "early" {
                 print("early")
                 
-                 event.Timeliness.append(0) //append a 0 to list
-                 event.Timeliness.remove(0) //pop first element to keep moving 5 day window
+                event.Timeliness.append(0) //append a 0 to list
+                event.Timeliness.remove(at: 0) //pop first element to keep moving 5 day window
                 
                 //event.OnTime = max(onTimeVal - 1,0)
                 
-                let lastTwoDays = suffix(event.Timeliness,2)
+                let lastTwoDays = event.Timeliness.suffix(from: 2)
                 
                 //if the user has been ontime 2 times in the last 2 days, decrement the notification schedule
                 if lastTwoDays.reduce(0, +) == 0{
                     event.OnTime = max(event.OnTime-1, 0)
                 }
-                
-                
-                
+            
             }
             
             else{
                 print("late")
                 event.Timeliness.append(1) //append a 1 to list
-                event.Timeliness.remove(0) //pop first element to keep moving 5 day window
-                               
+                event.Timeliness.remove(at: 0) //pop first element to keep moving 5 day window
+                
                 //if the user has been late 3 times in the last 5 days, decrement the notification schedule
-                if event.Timeliness.reduce(0, +) >=3{
+                if ( event.Timeliness.reduce(0, +) >= 3) {
+                    
+                    
                     event.OnTime = min(event.OnTime+1, 5)
                 }
             }
-            postEvent(event: event)  
+            postEvent(event: event)
         }
         
         else {
