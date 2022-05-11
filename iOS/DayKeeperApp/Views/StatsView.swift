ae8e6351cb3a63b3bb38967811bea7efda5fe718
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-05-11 00:00:02 -0400

    added time of day stats

--- iOS/DayKeeperApp/Views/StatsView.swift
+++ iOS/DayKeeperApp/Views/StatsView.swift
@@ -11,12 +11,12 @@ class StatViewModel : ObservableObject {
     @Published var name: String = ""
     var events : [Event] = getEventsFromDb()
     
-   
+    
     
     init() {
-         events = getEventsFromDb()
-        }
+        events = getEventsFromDb()
     }
+}
 
 
 
@@ -30,13 +30,13 @@ struct StatsView: View {
     init() {
         UITableView.appearance().backgroundColor = UIColor(Color(red:0.436, green: 0.558, blue: 0.925)) // Uses UIColor
     }
-      
+    
     
     var body: some View {
-       
+        
         
         ZStack {
-           
+            
             Text("Statistics")
                 .position(x: 200, y: 10)
                 .padding(.top, 50)
@@ -46,21 +46,21 @@ struct StatsView: View {
                 .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 30)!))
                 .minimumScaleFactor(0.5)
             
-            Text(String(worst_and_best_event()[0])).position(x: 300, y: 100)
+            Text(String(worst_and_best_event()[0])).position(x: 300, y: 120)
                 .shadow(radius: 15)
                 .foregroundColor(Color("Off-White"))
                 .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 20)!))
             
-            Text("Best Event").position(x: 300, y: 120)
-            .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
+            Text("Best Event").position(x: 300, y: 140)
+                .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
             
-            Text(String(worst_and_best_event()[1])).position(x: 300, y: 210)
+            Text(String(worst_and_best_event()[1])).position(x: 300, y: 180)
                 .shadow(radius: 15)
                 .foregroundColor(Color("Off-White"))
                 .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 20)!))
             
-            Text("Worst Event").position(x: 300, y: 230)
-            .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
+            Text("Worst Event").position(x: 300, y: 200)
+                .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
             
             Text(String(get_total_percentage()) + "%").position(x: 100, y: 150)
                 .shadow(radius: 15)
@@ -70,15 +70,36 @@ struct StatsView: View {
             
             Text("On Time").position(x: 100, y: 200)
                 .shadow(radius: 15)
-                .foregroundColor(Color("Off-White"))
                 .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 20)!))
-    
+            Group {
+                
+                let height = 250
+                let size = 30
+                let time_height = height + 30
+                
+                Text("Morning").position(x: 100, y: CGFloat(time_height)).font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
+                Text(String(worst_and_best_time()["morning"] ?? 0) + "%").position(x: 100, y: CGFloat(height))
+                    .shadow(radius: 15)
+                    .foregroundColor(Color("Off-White"))
+                    .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: CGFloat(size))!))
+                
+                Text("Afternoon").position(x: 200, y: CGFloat(time_height)).font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
+                Text(String(worst_and_best_time()["afternoon"] ?? 0) + "%").position(x: 200, y: CGFloat(height))
+                    .shadow(radius: 15)
+                    .foregroundColor(Color("Off-White"))
+                    .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: CGFloat(size))!))
+                
+                Text("Night").position(x: 300, y: CGFloat(time_height)).font(Font(uiFont: UIFont(name: "Lemon-Regular", size: 10)!))
+                Text(String(worst_and_best_time()["night"] ?? 0) + "%").position(x: 300, y: CGFloat(height))
+                    .shadow(radius: 15)
+                    .foregroundColor(Color("Off-White"))
+                    .font(Font(uiFont: UIFont(name: "Lemon-Regular", size: CGFloat(size))!))
+            }
             
-    
             
         } .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(red:0.436, green: 0.558, blue: 0.925))
-          
+        
         
     }
 }
@@ -92,7 +113,7 @@ struct StatsView_Previews: PreviewProvider {
 
 // of all possible events in the last 5 days, find out how many you were on time for
 func get_total_percentage() -> Int {
-
+    
     
     var sum = 0
     var  percentage = 0.0
@@ -102,23 +123,23 @@ func get_total_percentage() -> Int {
         sum += event.Timeliness.reduce(0, +)
         
     }
-//    print("event_count")
-   // print(event_count)
+    //    print("event_count")
+    // print(event_count)
     
-  //  print("SUM")
-  //  print(sum)
+    //  print("SUM")
+    //  print(sum)
     
     if event_count != 0 {
-     //   print("TEST event count")
-      //  print(53.0/103.0)
+        //   print("TEST event count")
+        //  print(53.0/103.0)
         percentage = Double(sum)/Double(event_count) * 100.0
-      //  print("Percentage")
+        //  print("Percentage")
         
-       // print(round(percentage))
+        // print(round(percentage))
         
     }
-
-
+    
+    
     return Int(round(percentage))
 }
 
@@ -141,22 +162,29 @@ func worst_and_best_event() -> Array<String>{
         
         return LHS.OnTime > RHS.OnTime
     })
-    print("PRINT SORTED EVENTS")
-    print(sorted_events)
+    // print("PRINT SORTED EVENTS")
+    //   print(sorted_events)
     worst_event = sorted_events.last!.Title
     best_event = sorted_events.first!.Title
-
+    
     
     
     return [worst_event, best_event]
 }
 
 
-func worst_and_best_time() -> Array<String> {
-    var time_array = [0, 0, 0]
+func worst_and_best_time() -> Dictionary<String, Int> {
+    var time_array = ["morning": 0,
+                      "afternoon": 0,
+                      "night": 0]
+    var total_array = ["morning": 0,
+                       "afternoon": 0,
+                       "night": 0]
     if getEventsFromDb().count == 0 {
         
-        return ["Add events!", "Add Events!", "Add Events!"]
+        return ["morning": 0,
+                "afternoon": 0,
+                "night": 0]
         
     }
     
@@ -166,25 +194,33 @@ func worst_and_best_time() -> Array<String> {
     
     for event in getEventsFromDb(){
         var hour = Int(formatter.string(from: event.StartDate))
-            
+        
         if hour! >= 4 && hour! <= 11 {
-            time_array[0] += event.Timeliness.reduce(0, +)
-        }
-            
-        if hour! >= 12 && hour! <= 19 {
-            time_array[1]  += event.Timeliness.reduce(0, +)
-                
-        }
-    
-        if hour! >= 19 || hour! <= 4 {
-            time_array[2]  += event.Timeliness.reduce(0, +)
-        }
+            time_array["morning"]! += event.Timeliness.reduce(0, +)
+            total_array["morning"]! += event.Timeliness.count
             
         }
         
-    
-    return [" "]
+        if hour! >= 12 && hour! <= 19 {
+            time_array["afternoon"]!  += event.Timeliness.reduce(0, +)
+            total_array["afternoon"]! += event.Timeliness.count
+        }
+        
+        if hour! >= 19 || hour! <= 4 {
+            time_array["night"]! += event.Timeliness.reduce(0, +)
+            total_array["night"]! += event.Timeliness.count
+        }
+        
     }
+    
+    time_array["morning"]! = Int(round(100.0 * Double(time_array["morning"]!)/Double(total_array["morning"]!)))
+    time_array["afternoon"]! = Int(round(100.0 * Double(time_array["afternoon"]!)/Double(total_array["afternoon"]!)))
+    time_array["night"]! = Int(round(100.0 * Double(time_array["night"]!)/Double(total_array["night"]!)))
+        
+    print(time_array)
+    return time_array
+    
+}
 
 
 func find_event_count() -> Int{
