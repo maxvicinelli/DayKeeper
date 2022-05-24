03d41e7f62ecd44b302028354ba4824b7a38a6af
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-05-24 12:30:46 -0400

    fixed up all bugs except driving time

--- iOS/DayKeeperApp/Models/locationNotifs.swift
+++ iOS/DayKeeperApp/Models/locationNotifs.swift
@@ -36,14 +36,14 @@ func initialize_location_notif_cycle(){
 
 func create_location_notif(event: Event){
     // check distance from current position and event, and the time it will take to leave
-    let driving_time = getDrivingTime(event: event)
+    let driving_time = getDrivingTime(event: event, completion: <#(Double?) -> Void#>)
     
     let diffComponents = Calendar.current.dateComponents([.second, .minute], from: Date(), to: event.StartDate)
     let time_to_event = diffComponents.second
     let minutes = diffComponents.minute
     var departure_time  = Calendar.current.date(
         byAdding: .second,
-        value: -1 * time_to_event,
+        value: -1 * time_to_event!,
         to: event.StartDate)
     // create timer to check location in (current time - event time)/2 minutes
     
@@ -53,7 +53,7 @@ func create_location_notif(event: Event){
     // Driving time = (z-y)
     // We want to check in (y-x)/2 time
     // (Y-x) = (z-x) - (Z-y)
-    let timer = Timer.scheduledTimer(withTimeInterval: (driving_time-time_to_event)/2.0, repeats: false) { timer in
+    let timer = Timer.scheduledTimer(withTimeInterval: Double((driving_time-time_to_event!)/2), repeats: false) { timer in
         print("Timer fired!")
         
         let new_driving_time = getDrivingTime(event: event)
@@ -63,12 +63,12 @@ func create_location_notif(event: Event){
             to: event.StartDate)
         
         //user is still on time
-        if ([Date()  compare: new_departure_time] == NSOrderedAscending) {
-            NSLog(@"current time is earlier than new departure time");
+        if (Date() < new_departure_time) {
+            NSLog("current time is earlier than new departure time");
             
             //user is running early and departure time has moved later
-            if ([departure_time  compare: new_departure_time] == NSOrderedDescending) {
-                NSLog(@"previous departure time is later than new departure time");
+            if (departure_time > new_departure_time) {
+                NSLog("previous departure time is later than new departure time");
                 
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "MMM d, h:mm a" // Sep 12, 2:11 PM --> MMM d, h:mm a
@@ -113,13 +113,13 @@ func create_location_notif(event: Event){
         
         
         // then create new timer with same rules, "recursively"
-        if ([event.StartDate  compare: new_departure_time] == NSOrderedDescending) {
-            NSLog(@"current time is earlier than new departure time");
+        if (event.StartDate < new_departure_time) {
+            NSLog("current time is earlier than new departure time");
             create_location_notif(event: Event)
         
     }
 }
-
+}
 func check_status(){
     // at event time, check location of user
     // update notification severity based on whether user is late or not
@@ -130,26 +130,27 @@ func check_status(){
 func sort_events_by_date() -> [Event] {
     let events = getEventsFromDb()
     
-    var dateFormatter = DateFormatter()
+    let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "dd MM, yyyy"// yyyy-MM-dd"
     
     for event in events {
-        let date = dateFormatter.date(from: event.StartDate)
+        let date = dateFormatter.date(from: dateFormatter.string(from: event.StartDate))
         if let date = date {
             event.StartDate = date
         }
     }
     
-    return  event.sorted(by: { $0.StartDate < $1.StartDate }) //ascending order
+    return  events.sorted(by: { $0.StartDate < $1.StartDate }) //ascending order
 }
 
 func find_soonest_event(events: [Event]) -> Event {
-    let soonest_event = events.first
+    var soonest_event = events.first!
     for event in events{
-        var timeInterval = events.StartDate.timeIntervalSinceDate(NSDate()) //find time interval from now till event
+        let timeInterval = event.StartDate.timeIntervalSince(NSDate() as Date)
+//find time interval from now till event
         
         if timeInterval > 0 {
-            let soonest_event = event
+            soonest_event = event
             break
         }
     }
@@ -210,3 +211,4 @@ func find_soonest_event(events: [Event]) -> Event {
         
         
     }
+
