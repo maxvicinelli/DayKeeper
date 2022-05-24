8609fe1acf67d6fdea5111fde77485f7b4c1e73c
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-05-24 11:11:43 -0400

    added more logic to location notification

--- iOS/DayKeeperApp/Models/locationNotifs.swift
+++ iOS/DayKeeperApp/Models/locationNotifs.swift
@@ -41,6 +41,10 @@ func create_location_notif(event: Event){
     let diffComponents = Calendar.current.dateComponents([.second, .minute], from: Date(), to: event.StartDate)
     let time_to_event = diffComponents.second
     let minutes = diffComponents.minute
+    var departure_time  = Calendar.current.date(
+        byAdding: .second,
+        value: -1 * time_to_event,
+        to: event.StartDate)
     // create timer to check location in (current time - event time)/2 minutes
     
     // Current time 10 am: x
@@ -51,9 +55,47 @@ func create_location_notif(event: Event){
     // (Y-x) = (z-x) - (Z-y)
     let timer = Timer.scheduledTimer(withTimeInterval: (driving_time-time_to_event)/2.0, repeats: false) { timer in
         print("Timer fired!")
-        // on timer conclusion, if user is on time don't do anything ,
-        // if user is late/ time to leave is earlier than before, update them
+        
+        let new_driving_time = getDrivingTime(event: event)
+        var new_departure_time  = Calendar.current.date(
+            byAdding: .second,
+            value: -1 * new_driving_time,
+            to: event.StartDate)
+        
+        //user is still on time
+        if ([Date()  compare: new_departure_time] == NSOrderedAscending) {
+            NSLog(@"current time is earlier than new departure time");
+            
+            //user is running early and departure time has moved later
+            if ([departure_time  compare: new_departure_time] == NSOrderedDescending) {
+                NSLog(@"previous departure time is later than new departure time");
+                
+                //notify user of new departure time
+                
+                
+            }
+            //user is running early but departure time has moved earlier
+            else{
+                
+                //do nothing
+                
+            }
+            
+        }
+        //user is late or just on time
+        else {
+            //tell user to leave now
+            
+        }
+        
+        
+        
         // then create new timer with same rules, "recursively"
+        if ([event.StartDate  compare: new_departure_time] == NSOrderedDescending) {
+            NSLog(@"event start time is later than new departure time");
+        
+        
+        
     }
 }
 
